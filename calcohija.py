9e8d5c27adcb9a3e940ee77ebc69b03579c05fbd
Jorge De Pablo Martinez <jdepablo@alpha28.aulas.gsyc.urjc.es>
2018-09-24 13:35:27 +0200

    calcohija no funcional, subsanar errores de herencia de clases

--- calcohija.py
+++ calcohija.py
@@ -5,4 +5,32 @@ import sys
 
 class CalculadoraHija(Calculadora)
 
-    def 
+    def times(self, op1, op2):
+        return op1 * op2
+
+    def over(self, op1, op2):
+        return op1 / op2
+
+calculator = CalculadoraHija(Calculadora)
+
+if __name__ == "__main__":
+    try:
+        operating1 = int(sys.argv[1])
+        operating2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    if sys.argv[2] == "suma":
+        result = calculator.plus(operating1, operating2)
+    elif sys.argv[2] == "resta":
+        result = calculator.minus(operating1, operating2)
+    elif sys.argv[2] == "por":
+        result = calculator.times(operating1, operating2)
+    elif sys.argv[2] == "entre":
+        if operating2 == 0:
+            sys.exit("Division by zero is not allowed")
+        else
+            result = calculator.over(operating1, operating2)
+
+    else:
+        sys.exit('Operación solo puede ser sumar,restar, multiplicar y dividir.')
