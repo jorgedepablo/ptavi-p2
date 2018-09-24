24c8e057e9c0005c6c6f5e3c5c5ae9cb2d3fa82b
Jorge De Pablo Martinez <jdepablo@gamma06.aulas.gsyc.urjc.es>
2018-09-24 10:47:04 +0200

    Intento añadir calculadora orientada a objetos

---
+++ calcoo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+    def plus(self, op1, op2):
+        return op1 + op2
+
+    def minus(self, op1, op2):
+        return op1 - op2
+
+calculator = Calculadora()
+
+if __name__ == "__main__":
+    try:
+        operating1 = int(sys.argv[1])
+        operating2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    if sys.argv[2] == "suma":
+        result = calculator.plus(operating1, operating2)
+    elif sys.argv[2] == "resta":
+        result = calculator.minus(operating1, operating2)
+    else:
+        sys.exit('Operación solo puede ser sumar o restar.')
+
+print(result)
