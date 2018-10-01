4c494fd173c79a9da94415942266955d7f73d62f
Jorge De Pablo Martinez <jdepablo@gamma06.aulas.gsyc.urjc.es>
2018-10-01 14:57:11 +0200

    funcional PEP8 NO pasado

--- calcplusplus.py
+++ calcplusplus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import sys
 import calcoohija
 import csv
@@ -7,11 +8,14 @@ import csv
 if __name__ == "__main__":
     try:
         with open(sys.argv[1], "r") as file:
-            operaciones = csv.reader(file)
-            print(operaciones)
-            #operando = operaciones[1]
-
+            line = csv.reader(file)
+            for operations in line:
+                arg = operations[0]
+                result = int(operations[1])
+                for operands in operations[2:]:
+                    calculator = calcoohija.CalculadoraHija(result, int(operands))
+                    result = calculator.operate(arg)
+                print(result)
+            file.close()
     except ValueError:
         sys.exit("Error: The argument is not valid")
-
-    file.close()
