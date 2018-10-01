cbef7034a1fcb0c5550e43661a5e71a53d026152
Jorge De Pablo Martinez <jdepablo@gamma06.aulas.gsyc.urjc.es>
2018-10-01 14:29:19 +0200

    funcional PEP8 pasado

--- calcplus.py
+++ calcplus.py
@@ -4,19 +4,20 @@
 import sys
 import calcoohija
 
+
 if __name__ == "__main__":
     try:
         file = open(sys.argv[1], "r")
-        for line in file.readlines():
-            line = line.rstrip('\n')
-            operations = line.split(',')
-            arg = operations[0]
-            result = int(operations[1])
-            for operands in operations[2:]:
-                calculator = calcoohija.CalculadoraHija(result, int(operands))
-                result = calculator.operate(arg)
-            print (result)
     except ValueError:
         sys.exit("Error: The argument is not valid")
 
+    for line in file.readlines():
+        line = line.rstrip('\n')
+        operations = line.split(',')
+        arg = operations[0]
+        result = int(operations[1])
+        for operands in operations[2:]:
+            calculator = calcoohija.CalculadoraHija(result, int(operands))
+            result = calculator.operate(arg)
+        print(result)
     file.close()
