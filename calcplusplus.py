64e904eada9f6916f85b0c2e0ca700e2a6099e4c
Jorge De Pablo Martinez <jdepablo@alpha27.aulas.gsyc.urjc.es>
2018-10-01 13:59:17 +0200

    problemas con paquete csv, primeras pruebas

--- calcplusplus.py
+++ calcplusplus.py
@@ -2,20 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 import calcoohija
+import csv
 
 if __name__ == "__main__":
     try:
-        file = open(sys.argv[1], "r")
-        for line in file.readlines():
-            line = line.rstrip('\n')
-            operations = line.split(',')
-            arg = operations[0]
-            result = int(operations[1])
-            for operands in operations[2:]:
-                calculator = calcoohija.CalculadoraHija(result, int(operands))
-                result = calculator.operate(arg)
+        with open(sys.argv[1], "r") as file:
+            operaciones = csv.reader(file)
+            print(operaciones)
+            #operando = operaciones[1]
 
-            print (result)
     except ValueError:
         sys.exit("Error: The argument is not valid")
 
